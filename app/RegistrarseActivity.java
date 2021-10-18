6212095af5f9b26c97d2a005c5a002435e199594
alexxq95 <alex_quijada@msn.com>
2021-10-18 12:53:55 -0700

    Clases para el registro e inicio de sesion

---
+++ app/RegistrarseActivity.java
@@ -0,0 +1,95 @@
+package com.ruddymiranda.helpmywoof;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegistrarseActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contrasena;
+    private EditText contrasenaconfirmacion;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        contrasenaconfirmacion = findViewById(R.id.contrasenaconfirmacion);
+
+
+
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrarusuario(View view){
+
+
+        if (contrasena.getText().toString().equals(contrasenaconfirmacion.getText().toString())){
+
+
+
+            mAuth.createUserWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user1 = FirebaseAuth.getInstance().getCurrentUser();
+                                user1.sendEmailVerification();
+
+
+
+                                Toast.makeText(getApplicationContext(), "Usuario creado.",
+                                        Toast.LENGTH_SHORT).show();
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                Intent i = new Intent(getApplicationContext(),AuthActivity.class);
+                                startActivity(i);
+
+
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }else {
+            Toast.makeText(this,"Las contraseñas no coinciden",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
