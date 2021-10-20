16088d68f8e327313ccd443216444757fe389ad0
alexxq95 <alex_quijada@msn.com>
2021-10-20 10:48:10 -0700

    Clases para el registro e inicio de sesion

---
+++ app/src/main/java/net/alecks/helpmewoof/IniciarSesionActivity.java
@@ -0,0 +1,79 @@
+package net.alecks.helpmewoof;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contrasena;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+        mAuth = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void iniciarSesion(View vie){
+        mAuth.signInWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+                            Toast.makeText(getApplicationContext(), "Autenticación correcta.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(),AuthActivity.class);
+                            startActivity(i);
+
+                            if (!user.isEmailVerified()){
+                                Toast.makeText(IniciarSesionActivity.this,"Correo electronico no verificado", Toast.LENGTH_LONG).show();
+                            }
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+}
