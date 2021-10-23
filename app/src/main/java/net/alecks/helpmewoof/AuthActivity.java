a2828b4036d76ea35147f31df5f5c55956672a74
alexxq95 <alex_quijada@msn.com>
2021-10-23 13:49:21 -0700

    Se agrego los permisos de la app

--- app/src/main/java/net/alecks/helpmewoof/AuthActivity.java
+++ app/src/main/java/net/alecks/helpmewoof/AuthActivity.java
@@ -3,12 +3,12 @@ package net.alecks.helpmewoof;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,14 +17,30 @@ import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthActivity extends AppCompatActivity {
+    String permisos[] = {Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+       requestPermissions(permisos, 1);
+
     }
+/*
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == 1){
+            for (int i = 0; i < permisos.length; i++) {
+                if(grantResults[i]== PackageManager.PERMISSION_GRANTED){
 
+                }
+            }
+        }
+    }
+*/
     public void iriniciar(View view){
         Intent i = new Intent(this, IniciarSesionActivity.class);
         startActivity(i);
--- app/src/main/java/net/alecks/helpmewoof/RegistrarseActivity.java
+++ app/src/main/java/net/alecks/helpmewoof/RegistrarseActivity.java
@@ -4,9 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
