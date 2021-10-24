f3d4fd9a2ec9796d238f8ebaf334b0e3de5ee445
VampireSqu1d <cuauhtemocayala99@gmail.com>
2021-10-23 20:25:41 -0700

    se agregó la activity maps

--- app/src/main/java/net/alecks/helpmewoof/MenuAcceso.java
+++ app/src/main/java/net/alecks/helpmewoof/MenuAcceso.java
@@ -42,7 +42,7 @@ public class MenuAcceso extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.registrarse, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
+                //.setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu_acceso);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
