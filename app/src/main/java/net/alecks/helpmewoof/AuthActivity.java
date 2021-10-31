1fcf08f652c672a0167e50237e315ef8f5de55d8
alexxq95 <alex_quijada@msn.com>
2021-10-31 02:04:12 -0700

    Se agregaron los botones de inicio de sesion y registro en el menu lateral

--- app/src/main/java/net/alecks/helpmewoof/AuthActivity.java
+++ app/src/main/java/net/alecks/helpmewoof/AuthActivity.java
@@ -1,20 +1,19 @@
 package net.alecks.helpmewoof;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.Manifest;
 import android.content.Intent;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import android.view.Menu;
 import android.view.View;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.navigation.NavigationView;
 
 public class AuthActivity extends AppCompatActivity {
     String permisos[] = {Manifest.permission.CAMERA,
@@ -22,28 +21,48 @@ public class AuthActivity extends AppCompatActivity {
                          Manifest.permission.READ_EXTERNAL_STORAGE,
                          Manifest.permission.INTERNET};
 
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-       requestPermissions(permisos, 1);
+        requestPermissions(permisos, 1);
 
+        elementosBarra();
+
+/*        NavigationView nav= findViewById(R.id.nav_menu);
+        nav.getMenu().clear();
+        nav.inflateMenu(R.menu.activity_main_drawer);*/
     }
-/*
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    //crea los elementos de la barra (nombre y boton)
+    public void elementosBarra(){
 
-        if (requestCode == 1){
-            for (int i = 0; i < permisos.length; i++) {
-                if(grantResults[i]== PackageManager.PERMISSION_GRANTED){
+        setSupportActionBar(findViewById(R.id.barrin));
 
+        drawerLayout = findViewById(R.id.main);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, findViewById(R.id.barrin), R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    /*
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            if (requestCode == 1){
+                for (int i = 0; i < permisos.length; i++) {
+                    if(grantResults[i]== PackageManager.PERMISSION_GRANTED){
+
+                    }
                 }
             }
         }
-    }
-*/
+    */
     public void iriniciar(View view){
         Intent i = new Intent(this, IniciarSesionActivity.class);
         startActivity(i);
@@ -54,5 +73,19 @@ public class AuthActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+
+    @Override
+    public void onBackPressed() {
+        drawerLayout = findViewById(R.id.main);
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 
 }
